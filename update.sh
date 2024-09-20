fa7424823eff152d36b93c4e346e2610b38d1099
RyyNotavailable <48169572+ryankputra@users.noreply.github.com>
2024-09-20 17:45:22 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -51,10 +51,10 @@ res1() {
 netfilter-persistent
 clear
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPT        \e[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPT RYYSTORE V2      \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e "  \033[1;91m update script service\033[1;37m"
+echo -e "  \033[1;91m update layanan script \033[1;37m"
 fun_bar 'res1'
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
