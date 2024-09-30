36f0547380b0ae289479f1b27e6fb74fadc017b0
RyyNotavailable <48169572+ryankputra@users.noreply.github.com>
2024-09-30 21:00:32 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -642,9 +642,9 @@ print_success "Fail2ban"
 function ins_epro(){
 clear
 print_install "Menginstall ePro WebSocket Proxy"
-wget -O /usr/bin/ws "https://wokszxdstore.net/ws/ws" >/dev/null 2>&1
-wget -O /usr/bin/tun.conf "https://wokszxdstore.net/ws/tun.conf" >/dev/null 2>&1
-wget -O /etc/systemd/system/ws.service "https://wokszxdstore.net/ws/ws.service" >/dev/null 2>&1
+wget -O /usr/bin/ws "https://raw.githubusercontent.com/RyyStore/x2621/files/ws" >/dev/null 2>&1
+wget -O /usr/bin/tun.conf "https://raw.githubusercontent.com/RyyStore/x2621/config/tun.conf" >/dev/null 2>&1
+wget -O /etc/systemd/system/ws.service "https://raw.githubusercontent.com/RyyStore/x2621/files/ws.service" >/dev/null 2>&1
 chmod +x /etc/systemd/system/ws.service
 chmod +x /usr/bin/ws
 chmod 644 /usr/bin/tun.conf
